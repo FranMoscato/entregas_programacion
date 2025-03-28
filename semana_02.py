d62319778299f5ea65d835db8f3c71dd93c20c3e
FranMoscato <moscatofranco04@gmail.com>
2025-03-27 21:45:10 -0300

    commit ej1

---
+++ semana_02.py
@@ -0,0 +1,14 @@
+#ejercicio 1
+
+def invertir_lista(lista):
+    lista_r=lista.copy()[::-1]
+    return lista_r
+
+a=[1, 2, 3, 4, 5]
+b=['Bogotá', 'Rosario', 'San Fernando', 'San Miguel']
+print(a,"-->",invertir_lista(a))
+print(b,"-->",invertir_lista(b))
+
+#ejercicio 2
+
+
