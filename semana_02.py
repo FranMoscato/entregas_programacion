909d614d1c4c5a26393d8bcfa3e420b8d4f9a5d7
FranMoscato <moscatofranco04@gmail.com>
2025-04-02 15:42:10 -0300

    ejercicio 2 termiando

--- semana_02.py
+++ semana_02.py
@@ -4,11 +4,33 @@ def invertir_lista(lista):
     lista_r=lista.copy()[::-1]
     return lista_r
 
-a=[1, 2, 3, 4, 5]
-b=['Bogotá', 'Rosario', 'San Fernando', 'San Miguel']
-print(a,"-->",invertir_lista(a))
-print(b,"-->",invertir_lista(b))
+#ejercicio 2
+def collatz(nro):
+    var=nro
+    count=0
+    while var!=1:
+        if var%2==0:
+            var=var/2
+        else:
+            var=var*3+1
+        count+=1
+    return count
 
 #ejercicio 2
+def collatz(nro):
+        var=nro
+        count=0
+        while var!=1:
+            if var%2==0:
+                var=var/2
+            else:
+                var=var*3+1
+            count+=1
+        return count
 
+print(collatz(1))
+print(collatz(2))
+print(collatz(3))
+
+    
 
